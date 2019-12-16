148deecb074a7c23fbc6018f3944f1c7ed03e679
michael hunt <milomia@michaels-iMac-4.local>
2019-12-16 11:20:57 +0000

    update database def

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -69,6 +69,6 @@ class InterviewsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def interview_params
-      params.require(:interview).permit(:company_name, :date, :stage, :method, :verdict)
+      params.require(:interview).permit(:company_name, :date, :stage, :method, :verdict, :contact)
     end
 end
